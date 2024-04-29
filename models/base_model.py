e640cc13fd551f3c8c3928e99df47440916af9b0
YOUSSEF <youssef.acherki@um5r.ac.ma>
2024-04-29 16:06:32 -0700

    airbnbclone

--- models/base_model.py
+++ models/base_model.py
@@ -14,8 +14,7 @@ class BaseModel:
     id = Column(String(60), primary_key=True, nullable=False)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    
-    """A base class for all hbnb models"""
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -57,8 +56,7 @@ class BaseModel:
         if '_sa_instance_state' in dictionary.keys():
             del dictionary['_sa_instance_state']
         return dictionary
-    
+
     def delete(self):
         '''delete the current instance from the storage'''
         models.storage.delete()
-
