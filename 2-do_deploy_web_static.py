265facf65e23d05210e6461c1c59774a0f892075
ABDELHAMID El Yaagoubi <abdelhamidbusiness@gmail.com>
2024-05-17 12:23:53 +0100

    working

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -20,7 +20,8 @@ def do_deploy(archive_path):
         # Upload the archive to the /tmp/ directory of the web server
         put(archive_path, '/tmp/')
 
-        # Uncompress the archive to the folder /data/web_static/releases/<archive filename without extension>
+        # Uncompress the archive to the folder
+        # /data/web_static/releases/<archive filename without extension>
         file_name = archive_path.split('/')[-1]
         file_folder = '/data/web_static/releases/' + file_name[:-4] + '/'
         run('mkdir -p {}'.format(file_folder))
@@ -40,8 +41,8 @@ def do_deploy(archive_path):
 
         # Create a new symbolic link linked to the new version of your code
         run('ln -s {} /data/web_static/current'.format(file_folder))
+        print("New version deployed!")
         return True
     except Exception as e:
         print(e)
         return False
-
